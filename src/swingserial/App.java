3870ff065244dfb72676e3e5351fd3176bb7490a
Lukas <lukas.becker.lb@gmail.com>
2019-05-24 18:04:16 +0200

    removed waring in App.java

--- src/swingserial/App.java
+++ src/swingserial/App.java
@@ -5,15 +5,13 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args){
 
-        SwingUtilities.invokeLater(new Runnable() { //Use the Thread for Swing
-            @Override
-            public void run() {
-                myFrame frame = new myFrame(); //Use the design we made
-                frame.setVisible(true); //make the window visible
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //if someone closes the APP, actually close it
-                frame.setSize(500,500); //Set the Size
-                frame.setTitle("Serial Communicator"); //Set the Title
-            }
+        //Use the Thread for Swing
+        SwingUtilities.invokeLater(() -> {
+            myFrame frame = new myFrame(); //Use the design we made
+            frame.setVisible(true); //make the window visible
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //if someone closes the APP, actually close it
+            frame.setSize(500,500); //Set the Size
+            frame.setTitle("Serial Communicator"); //Set the Title
         });
 
 
